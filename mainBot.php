28aa85ff4a4035256bd01e43702cab4d4400020a
primo6711 <bchukwu@yahoo.com>
2017-01-27 18:57:36 -0500

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token = "48ce04954e1d444e1830ef867d";
 
 include('./index.php');
 
